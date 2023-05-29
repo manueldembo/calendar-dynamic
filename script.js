93add926cb0f65ed71036cc55fdb619bb5d2660a
manueldembo <madembo18@gmail.com>
2023-05-29 23:43:09 +0100

    refactor: organize the code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const prevNextIcon = document.querySelectorAll(".icons span");
 let date = new Date();
 let currentYear = date.getFullYear();
 let currentMonth = date.getMonth();
+
 const months = [
   "January",
   "February",
@@ -21,6 +22,80 @@ const months = [
 ];
 
 const renderCalendar = () => {
+  const liTag = buildCalendarDays();
+  currentDate.innerHTML = `${months[currentMonth]} ${currentYear}`;
+  daysTag.innerHTML = liTag;
+};
+
+prevNextIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    if (icon.id == "prev") {
+      currentMonth -= 1;
+    }
+
+    if (icon.id === "next") {
+      currentMonth += 1;
+    }
+
+    date = new Date();
+
+    if (currentMonth < 0 || currentMonth > 11) {
+      changeYear();
+    }
+
+    renderCalendar();
+  });
+});
+
+function changeYear() {
+  date = new Date(currentYear, currentMonth);
+  currentYear = date.getFullYear();
+  currentMonth = date.getMonth();
+}
+
+function getPreviusMonthLastDays(firstDayOfMonth, lastDateOfLastMonth) {
+  let liTag = "";
+
+  for (let i = firstDayOfMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
+  }
+
+  return liTag;
+}
+
+function getNextMonthFistDays(lastDayOfMonth) {
+  let liTag = "";
+
+  for (let i = lastDayOfMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
+  }
+
+  return liTag;
+}
+
+function getAllDaysOfCurrentMonth(lastDateOfMonth) {
+  let liTag = "";
+
+  for (let i = 1; i <= lastDateOfMonth; i++) {
+    const state = isToday(i);
+    liTag += `<li class="${state}">${i}</li>`;
+  }
+
+  return liTag;
+}
+
+function isToday(day) {
+  if (
+    day === date.getDate() && currentMonth === new Date().getMonth() &&
+    currentYear === new Date().getFullYear()
+  ) {
+    return "active";
+  }
+
+  return "";
+}
+
+function buildCalendarDays() {
   let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
   let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   let lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth)
@@ -28,41 +103,11 @@ const renderCalendar = () => {
   let lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate();
   let liTag = "";
 
-  for (let i = firstDayOfMonth; i > 0; i--) {
-    liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
-  }
+  liTag += getPreviusMonthLastDays(firstDayOfMonth, lastDateOfLastMonth);
+  liTag += getAllDaysOfCurrentMonth(lastDateOfMonth);
+  liTag += getNextMonthFistDays(lastDayOfMonth);
 
-  for (let i = 1; i <= lastDateOfMonth; i++) {
-    let isToday =
-      i === date.getDate() && currentMonth === new Date().getMonth() &&
-        currentYear === new Date().getFullYear()
-        ? "active"
-        : "";
-    liTag += `<li class="${isToday}">${i}</li>`;
-  }
-
-  for (let i = lastDayOfMonth; i < 6; i++) {
-    liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
-  }
-
-  currentDate.innerHTML = `${months[currentMonth]} ${currentYear}`;
-  daysTag.innerHTML = liTag;
-};
-
-prevNextIcon.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
-
-    if (currentMonth < 0 || currentMonth > 11) {
-        date = new Date(currentYear, currentMonth);
-        currentYear = date.getFullYear();
-        currentMonth = date.getMonth();
-    } else {
-        date = new Date();
-    }
-
-    renderCalendar();
-  });
-});
+  return liTag;
+}
 
 renderCalendar();
